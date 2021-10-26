becc72c7bd3172148fb402dc7fd28ba16b5d4ea7
sinkland <306463830@qq.com>
2021-10-26 23:32:37 +0800

    更换显示字体

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -11,12 +11,9 @@ void start_kernel(void)
         addr[i] = 0x0099ffcc;
     }
 
-    character_print_color(addr, 1440, 50, 50, 'a', 0x00000000);
-    character_print_color(addr, 1440, 51, 50, 'a', 0x00000000);
-    character_print_color(addr, 1440, 51, 51, 'a', 0x00000000);
-
     string_print_color(addr, 1440, 101, 200, "abcdefghijklmnopqrstuvwxyz", 0x00000000);
-    string_print_color(addr, 1440, 101, 216, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 0x00000000);
+    string_print_color(addr, 1440, 101, 224, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 0x00000000);
+
     while (1)
     {
 
